5803b2600b1739a1c23f66d1d2fa25ca7484e0eb
Luis Elías Carro <luis.carro@gmail.com>
2021-05-16 14:19:41 -0300

    Encriptacion simetrica DESCryptoServiceProvider

--- Mcba.Infraestruture/HashHelper.cs
+++ Mcba.Infraestruture/HashHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Text;
+using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace Mcba.Infraestruture
 {
@@ -31,6 +32,68 @@ namespace Mcba.Infraestruture
         {
             byte[] b = Convert.FromBase64String(word);
             return Encoding.UTF8.GetString(b);
+        }
+
+        public static byte[] EncryptCsp(string data)
+        {
+            // Create a new DESCryptoServiceProvider object
+            // to generate a key and initialization vector (IV).
+            DESCryptoServiceProvider cryptProvider = new DESCryptoServiceProvider();
+            byte[] key = cryptProvider.Key;
+            byte[] iv = cryptProvider.IV;
+
+            byte[] ret;
+
+            // Create a MemoryStream.
+            using (MemoryStream mStream = new MemoryStream())
+            {
+                // Create a CryptoStream using the MemoryStream
+                // and the passed key and initialization vector (IV).
+                using (CryptoStream cStream = new CryptoStream(mStream, new DESCryptoServiceProvider().CreateEncryptor(key, iv), CryptoStreamMode.Write))
+                {
+                    // Convert the passed string to a byte array.
+                    byte[] toEncrypt = new ASCIIEncoding().GetBytes(data);
+
+                    // Write the byte array to the crypto stream and flush it.
+                    cStream.Write(toEncrypt, 0, toEncrypt.Length);
+                    cStream.FlushFinalBlock();
+
+                    // Get an array of bytes from the
+                    // MemoryStream that holds the
+                    // encrypted data.
+                    ret = mStream.ToArray();
+
+                    // Close the streams.
+                    cStream.Close();
+                    mStream.Close();
+                }
+            }
+
+            // Return the encrypted buffer.
+            return ret;
+        }
+
+        public static string DecryptCsp(byte[] data, byte[] key, byte[] iv)
+        {
+            // Create a new MemoryStream using the passed
+            // array of encrypted data.
+            using (MemoryStream msDecrypt = new MemoryStream(data))
+            {
+                // Create a CryptoStream using the MemoryStream
+                // and the passed key and initialization vector (IV).
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, new DESCryptoServiceProvider().CreateDecryptor(key, iv), CryptoStreamMode.Read))
+                {
+                    // Create buffer to hold the decrypted data.
+                    byte[] fromEncrypt = new byte[data.Length];
+
+                    // Read the decrypted data out of the crypto stream
+                    // and place it into the temporary buffer.
+                    csDecrypt.Read(fromEncrypt, 0, fromEncrypt.Length);
+
+                    //Convert the buffer into a string and return it.
+                    return new ASCIIEncoding().GetString(fromEncrypt);
+                }
+            }
         }
     }
 }
